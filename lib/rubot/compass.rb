f9f81fc130fa1872dadd108e794733de898c6a59
Paul Carey <paul@sourcelab.technology>
2017-04-03 16:04:33 +0930

    Implements Rubot::Compass#orientation= to set angle based on direction/orientation input

--- lib/rubot/compass.rb
+++ lib/rubot/compass.rb
@@ -3,8 +3,19 @@ module Rubot
 
     attr_accessor :angle
 
+    ORIENTATION_MAP = {
+        north: 0,
+        east:  90,
+        south: 180,
+        west:  270
+    }
+
     def initialize(start_angle=0)
       @angle = start_angle
+    end
+
+    def orientation=(new_orientation)
+      @angle =  ORIENTATION_MAP[new_orientation]
     end
   end
 end
--- spec/lib/rubot/compass_spec.rb
+++ spec/lib/rubot/compass_spec.rb
@@ -14,4 +14,31 @@ describe Rubot::Compass do
       expect(compass.angle).to eq(0)
     end
   end
+
+  describe 'orientation=' do
+
+    context 'valid orientation provided' do
+
+      let(:compass) { Rubot::Compass.new(360) }
+
+      context 'symbol provided as input' do
+
+        it 'sets angle to 0 for :north' do
+          expect { compass.orientation=:north }.to change { compass.angle }.to(0)
+        end
+
+        it 'sets angle to 90 for :east' do
+          expect { compass.orientation=:east }.to change { compass.angle }.to(90)
+        end
+
+        it 'sets angle to 180 for :south' do
+          expect { compass.orientation=:south }.to change { compass.angle }.to(180)
+        end
+
+        it 'sets angle to 270 for :west' do
+          expect { compass.orientation=:west }.to change { compass.angle }.to(270)
+        end
+      end
+    end
+  end
 end
